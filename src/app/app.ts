403307094bd3fc384b4003a1e01b22729f6be19a
daniel <tamdiengtchabdaguyalain@gmail.com>
2026-09-12 15:03:41 +0100

    internationalisation avec i18n

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule],
+  imports: [FormsModule, TranslatePipe],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
@@ -11,6 +12,12 @@ export class App {
   protected readonly title = signal('cypress-sandbox');
   protected readonly todos = signal<string[]>([]);
   protected newTodo = '';
+
+  constructor(private translate: TranslateService) {}
+
+  switchLang(lang: string) {
+    this.translate.use(lang);
+  }
 
   addTodo() {
     if (this.newTodo.trim()) {
